2b650a0863fee9ac55c583dd548eecde918da3d4
Viktoriya-max <viktoriyakoloskova20@gmail.com>
2024-10-03 22:40:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+exampl='Колосков Максим'
+#1st exercise
+print(exampl[0])
+#2sr exercise
+print(exampl[-1])
+#3st exercise
+print(exampl[8:])
+#4st exercise
+print(exampl[::-1])
+print(exampl[1::2])
